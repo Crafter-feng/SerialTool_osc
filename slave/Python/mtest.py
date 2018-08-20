f6bfd6e347df7b0a2dbdf44cc8766530d799a6fb
rose-w <weiyanfengv@gmail.com>
2018-08-20 10:44:13 +0800

    完善说明

--- slave/Python/mtest.py
+++ slave/Python/mtest.py
@@ -6,12 +6,12 @@ import urandom
 import time
 
 uart = UART(2, baudrate=115200, rx=13,tx=12,timeout=10)
-osc = sendwave.SendWave(uart,2)
 
-for i in range(1000):
-    value = urandom.randint(20, 2000)
-    print(value)
-    data = osc.ws_point(0,value)    
-    print(data)
-    serial.write(data)
+osc = sendwave.SendWave()
+
+for i in range(100):
+    value = [urandom.randint(20, 2000) for x in range(16)]
+    data = osc.ws_point(*value) 
+    #data = osc.ws_sync(*value)        
+    uart.write(data)
     time.sleep(0.1)
